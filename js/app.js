557a103be343c37c022f107588ca26307335ac00
Laura McCartney <hello@lauramccartney.co.uk>
2013-05-27 20:43:33 +0100

    Add input box

--- js/app.js
+++ js/app.js
@@ -2,8 +2,17 @@ function getImageURL(){
     return "url(" + window.location.search.substring(1) + ")";
 }
 
+function updateURL(){
+    window.location.search = $("#typebox").val();
+}
+
 
 $(function() {
     $("#simple_sketch").css("background-image", getImageURL());
     $('#simple_sketch').sketch({defaultColor: "#564023", defaultSize: "15px"});
 });
+
+
+// write function that takes one argument 
+// updates the query string with contents of one argument 
+// redirects user to url
