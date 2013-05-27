acac22485694cd840b894eae2b9f919ea1c2b374
Laura McCartney <hello@lauramccartney.co.uk>
2013-05-27 21:10:08 +0100

    Make input box fire on submit

--- js/app.js
+++ js/app.js
@@ -2,17 +2,14 @@ function getImageURL(){
     return "url(" + window.location.search.substring(1) + ")";
 }
 
-function updateURL(){
-    window.location.search = $("#typebox").val();
-}
+$("#inputbox").submit(
+    function updateURL(){
+        window.location.search = "?" + $("#typebox").val();
+        return false;
+});
 
 
 $(function() {
     $("#simple_sketch").css("background-image", getImageURL());
     $('#simple_sketch').sketch({defaultColor: "#564023", defaultSize: "15px"});
 });
-
-
-// write function that takes one argument 
-// updates the query string with contents of one argument 
-// redirects user to url
